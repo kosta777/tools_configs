396d9c4828bbd2d15038fb0d8548c2ffdc050db9
Kosta Stojiljkovic <kostasto@proton.me>
2025-08-05 22:26:36 +0200

    Adding link scripts and cleanup

---
+++ link_nvim_config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Get the full path of the dir containing this script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+NVIM_DIR="$SCRIPT_DIR/nvim"
+HOME_DIR="$HOME"
+
+# Create the symlink
+ln -s "$NVIM_DIR" "$HOME_DIR/.config/nvim"
+
